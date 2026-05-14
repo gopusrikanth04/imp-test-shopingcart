80d76d96fb7eb41ac82e7e3095940a6fd738f0d7
Srikanth Gopu <srikanth.g@releaseowl.com>
2026-05-14 12:56:37 +0000

    sample initial project

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.cart.controller.View1", {
+        onInit() {
+        }
+    });
+});
